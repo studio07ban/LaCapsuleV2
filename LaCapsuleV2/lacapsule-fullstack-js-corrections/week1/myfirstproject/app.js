d74b2e83be9fd1f0f0e5b01298e28f9733335946
Your namepierreBeauchesne <pierre.beauchesne@gmail.fr>
2025-10-15 16:41:10 +0200

    LaCapsuleV2

---
+++ LaCapsuleV2/lacapsule-fullstack-js-corrections/week1/myfirstproject/app.js
@@ -0,0 +1,10 @@
+function getPerfectNumber() {
+  const perfectNumber = 09;
+ 
+  return perfectNumber;
+ }
+
+console.log(getPerfectNumber());
+
+// Do not edit/remove code under this line
+module.exports = getPerfectNumber;
